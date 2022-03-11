9a10376210fe3999f8bc94abdb1176c868d67158
hizashi <zytwp@hotmail.com>
2022-03-11 13:49:47 +0800

    solved day2 part 2

--- day2/day2.js
+++ day2/day2.js
@@ -3,22 +3,40 @@ exports.__esModule = true;
 var fs = require("fs");
 var inputData = fs.readFileSync("day2.txt", "utf-8");
 var splitData = inputData.split("\n");
-console.log(splitData);
 var horizontal = 0;
 var depth = 0;
+// for (let i = 0; i < splitData.length; i++) {
+//   let currentArr: Array<string> = splitData[i].split(" ");
+//   let command: string = currentArr[0];
+//   let units: number = parseInt(currentArr[1]);
+//   if (command === "forward") {
+//     horizontal += units;
+//   } else if (command === "down") {
+//     depth += units;
+//   } else {
+//     depth -= units;
+//   }
+// }
+// const multiplied: number = horizontal * depth;
+// console.log(horizontal, depth, multiplied);
+//horizontal = 1911, depth = 724 and multiplying them together gives 1383564
+var aim = 0;
+//down increases aim, up decreases aim
+//forward increases horizontal AND increase depth (current aim * X)
 for (var i = 0; i < splitData.length; i++) {
     var currentArr = splitData[i].split(" ");
     var command = currentArr[0];
     var units = parseInt(currentArr[1]);
-    if (command === "forward") {
-        horizontal += units;
+    if (command === "down") {
+        aim += units;
     }
-    else if (command === "down") {
-        depth += units;
+    else if (command === "up") {
+        aim -= units;
     }
     else {
-        depth -= units;
+        horizontal += units;
+        depth += aim * units;
     }
 }
-var multiplied = horizontal * depth;
-console.log(horizontal, depth, multiplied);
+var multiplied2 = horizontal * depth;
+console.log(multiplied2);
--- day2/day2.ts
+++ day2/day2.ts
@@ -6,19 +6,41 @@ const splitData: Array<string> = inputData.split("\n");
 var horizontal: number = 0;
 var depth: number = 0;
 
+// for (let i = 0; i < splitData.length; i++) {
+//   let currentArr: Array<string> = splitData[i].split(" ");
+//   let command: string = currentArr[0];
+//   let units: number = parseInt(currentArr[1]);
+//   if (command === "forward") {
+//     horizontal += units;
+//   } else if (command === "down") {
+//     depth += units;
+//   } else {
+//     depth -= units;
+//   }
+// }
+
+// const multiplied: number = horizontal * depth;
+// console.log(horizontal, depth, multiplied);
+//horizontal = 1911, depth = 724 and multiplying them together gives 1383564
+
+var aim: number = 0;
+//down increases aim, up decreases aim
+//forward increases horizontal AND increase depth (current aim * X)
+
 for (let i = 0; i < splitData.length; i++) {
   let currentArr: Array<string> = splitData[i].split(" ");
   let command: string = currentArr[0];
   let units: number = parseInt(currentArr[1]);
-  if (command === "forward") {
-    horizontal += units;
-  } else if (command === "down") {
-    depth += units;
+  if (command === "down") {
+    aim += units;
+  } else if (command === "up") {
+    aim -= units;
   } else {
-    depth -= units;
+    horizontal += units;
+    depth += aim * units;
   }
 }
 
-const multiplied: number = horizontal * depth;
-console.log(horizontal, depth, multiplied);
-//horizontal = 1911, depth = 724 and multiplying them together gives 1383564
+const multiplied2: number = horizontal * depth;
+console.log(multiplied2);
+//answer is 1488311643
