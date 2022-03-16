80ef8a2c17032b476e963c883893541b014abfe8
hizashi <zytwp@hotmail.com>
2022-03-16 21:56:45 +0800

    day3part2 done, this shite took me awhile

--- day3/day3.js
+++ day3/day3.js
@@ -12,12 +12,10 @@ for (var i = 0; i < splitData.length; i++) {
         }
     }
 }
-console.log(counts);
 var half = splitData.length / 2;
 var gamma = "";
 var epsilon = "";
 for (var k = 0; k < counts.length; k++) {
-    console.log(counts[k], half);
     if (counts[k] > half) {
         gamma = gamma + "1";
         epsilon = epsilon + "0";
@@ -31,3 +29,82 @@ var gammaDecimal = parseInt(gamma, 2);
 var epsilonDecimal = parseInt(epsilon, 2);
 var multipled = gammaDecimal * epsilonDecimal;
 console.log(multipled);
+//answer for part 1 is 3912944
+console.log(gamma);
+//part2
+var currentData = splitData;
+var placing = 0;
+var oxygenGeneratorRating = function (data) {
+    var oneArr = [];
+    var zeroArr = [];
+    if (data.length == 1) {
+        return;
+    }
+    for (var i = 0; i < data.length; i++) {
+        var currentElement = data[i];
+        if (currentElement[placing] == "1") {
+            oneArr.push(data[i]);
+        }
+        else if (currentElement[placing] == "0") {
+            zeroArr.push(data[i]);
+        }
+    }
+    console.log(oneArr.length, zeroArr.length);
+    console.log(oneArr, zeroArr);
+    if (oneArr.length == zeroArr.length) {
+        currentData = oneArr;
+        placing += 1;
+    }
+    else if (oneArr.length > zeroArr.length) {
+        currentData = oneArr;
+        placing += 1;
+    }
+    else if (zeroArr.length > oneArr.length) {
+        currentData = zeroArr;
+        placing += 1;
+    }
+    if (placing < 12) {
+        oxygenGeneratorRating(currentData);
+    }
+};
+oxygenGeneratorRating(currentData);
+var oxygenGeneratorRatingDecimal = parseInt(currentData[0], 2);
+console.log(oxygenGeneratorRatingDecimal);
+currentData = splitData;
+placing = 0;
+var co2ScrubberRating = function (data) {
+    var oneArr = [];
+    var zeroArr = [];
+    if (data.length == 1) {
+        return;
+    }
+    for (var i = 0; i < data.length; i++) {
+        if (data[i][placing] == "1") {
+            oneArr.push(data[i]);
+        }
+        else {
+            zeroArr.push(data[i]);
+        }
+    }
+    console.log(oneArr.length, zeroArr.length);
+    console.log(oneArr, zeroArr);
+    if (oneArr.length == zeroArr.length) {
+        console.log(oneArr.length, zeroArr.length, "same length");
+        currentData = zeroArr;
+        placing += 1;
+    }
+    else if (oneArr.length > zeroArr.length) {
+        currentData = zeroArr;
+        placing += 1;
+    }
+    else if (zeroArr.length > oneArr.length) {
+        currentData = oneArr;
+        placing += 1;
+    }
+    if (placing < 12) {
+        co2ScrubberRating(currentData);
+    }
+};
+co2ScrubberRating(currentData);
+var co2ScrubberRatingDecimal = parseInt(currentData[0], 2);
+console.log(oxygenGeneratorRatingDecimal * co2ScrubberRatingDecimal);
--- day3/day3.ts
+++ day3/day3.ts
@@ -1,3 +1,4 @@
+import { on } from "events";
 import * as fs from "fs";
 
 const inputData: string = fs.readFileSync("day3.txt", "utf-8");
@@ -18,7 +19,6 @@ const half: number = splitData.length / 2;
 var gamma: string = "";
 var epsilon: string = "";
 for (let k = 0; k < counts.length; k++) {
-  console.log(counts[k], half);
   if (counts[k] > half) {
     gamma = gamma + "1";
     epsilon = epsilon + "0";
@@ -32,3 +32,87 @@ const epsilonDecimal: number = parseInt(epsilon, 2);
 const multipled = gammaDecimal * epsilonDecimal;
 console.log(multipled);
 //answer for part 1 is 3912944
+console.log(gamma);
+
+//part2
+let currentData: Array<string> = splitData;
+let placing: number = 0;
+
+const oxygenGeneratorRating = (data: Array<string>) => {
+  let oneArr: Array<string> = [];
+  let zeroArr: Array<string> = [];
+  if (data.length == 1){
+    return
+  }
+  for (let i = 0; i < data.length; i++) {
+    let currentElement: string = data[i];
+    if (currentElement[placing] == "1") {
+      oneArr.push(data[i]);
+    } else if (currentElement[placing] == "0") {
+      zeroArr.push(data[i]);
+    }
+  }
+  console.log(oneArr.length, zeroArr.length);
+  console.log(oneArr, zeroArr);
+  if (oneArr.length == zeroArr.length) {
+    currentData = oneArr;
+    placing += 1;
+  } else if (oneArr.length > zeroArr.length) {
+    currentData = oneArr;
+    placing += 1;
+  } else if (zeroArr.length > oneArr.length) {
+    currentData = zeroArr;
+    placing += 1;
+  }
+  if (placing < 12) {
+    oxygenGeneratorRating(currentData);
+  }
+};
+
+oxygenGeneratorRating(currentData);
+const oxygenGeneratorRatingDecimal: number = parseInt(currentData[0], 2);
+
+console.log(oxygenGeneratorRatingDecimal)
+
+currentData = splitData;
+placing = 0;
+
+const co2ScrubberRating = (data: Array<string>) => {
+  let oneArr: Array<string> = [];
+  let zeroArr: Array<string> = [];
+
+  if (data.length == 1){
+    return
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][placing] == "1") {
+      oneArr.push(data[i]);
+    } else {
+      zeroArr.push(data[i]);
+    }
+  }
+
+  console.log(oneArr.length, zeroArr.length);
+  console.log(oneArr, zeroArr);
+  
+  if (oneArr.length == zeroArr.length) {
+    console.log(oneArr.length, zeroArr.length, "same length");
+    currentData = zeroArr;
+    placing += 1
+  } else if (oneArr.length >zeroArr.length) {
+    currentData = zeroArr;
+    placing += 1;
+  } else if (zeroArr.length > oneArr.length){
+    currentData = oneArr;
+    placing += 1;
+  }
+  if (placing < 12) {
+    co2ScrubberRating(currentData);
+  }
+};
+co2ScrubberRating(currentData);
+
+const co2ScrubberRatingDecimal: number = parseInt(currentData[0], 2);
+
+console.log(oxygenGeneratorRatingDecimal * co2ScrubberRatingDecimal);
