847915bf2d1e200e0afe61444cd28fe2f7f1d7ff
hizashi <zytwp@hotmail.com>
2022-03-24 10:58:51 +0800

    still on d4

--- day4/day4.ts
+++ day4/day4.ts
@@ -6,22 +6,55 @@ const splitData: Array<string> = inputData.split("\n");
 
 //array of the numbers drawn
 const drawNumbers: string[] = splitData[0].split(",");
-console.log(drawNumbers)
+console.log(drawNumbers);
 
 //remove the numbers drawn and the first empty line from the initial data
-splitData.splice(0,2)
-console.log(splitData)
+splitData.splice(0, 2);
+console.log(splitData);
 //re-organize the card storage data and add arrays for horizontal lines
 // const allCards: string[][][];
-let h1: string[] = splitData[0].split(" ").filter(el => el != "")
-let h2: string[] = splitData[1].split(" ").filter(el => el != "")
-let h3: string[] = splitData[2].split(" ").filter(el => el != "")
-let h4: string[] = splitData[3].split(" ").filter(el => el != "")
-let h5: string[] = splitData[4].split(" ").filter(el => el != "")
-let arr: string[][] = [h1,h2,h3,h4,h5]
-console.log(arr)
-let v1: string[] = [h1[0],h2[0],h3[0],h4[0],h5[0]]
-console.log(v1)
-// for (let i = 0; i < splitData.length; i++) {
-//     let h1: string[] = splitData[i].split(" ")
-// }
+
+let arr: string[][] = [h1, h2, h3, h4, h5];
+console.log(arr);
+
+const createCard = (arr: string[]) => {
+  let h1: string[] = splitData[0].split(" ").filter((el) => el != "");
+  let h2: string[] = splitData[1].split(" ").filter((el) => el != "");
+  let h3: string[] = splitData[2].split(" ").filter((el) => el != "");
+  let h4: string[] = splitData[3].split(" ").filter((el) => el != "");
+  let h5: string[] = splitData[4].split(" ").filter((el) => el != "");
+  let count: number = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let card: string[] = [];
+    if (arr[i] == "") {
+      console.log("an empty line was detected");
+      continue;
+    }
+    h1 = arr[i].split(" ").filter((el) => el != "");
+  }
+};
+
+const isDrawn = (currentValue: string) => {
+  currentValue = "-";
+};
+let lastDrawnNumber: string = ""
+let winningCard: string[] = [];
+const checkWin = (singleCardArray: string[][]) => {
+    //check horizontals
+  for (let i = 0; i < singleCardArray.length; i++) {
+    if (singleCardArray[i].every(isDrawn)) {
+      winningCard = singleCardArray[i];
+      console.log(winningCard)
+      return
+    }
+  }
+
+  //check verticals
+  let v1: string[] = [singleCardArray[0][0],singleCardArray[1][0],singleCardArray[2][0],singleCardArray[3][0],singleCardArray[4][0]]
+  let v2: string[] = []
+  let v3: string[] = []
+  let v4: string[] = []
+  let v5: string[] = []
+
+
+};
