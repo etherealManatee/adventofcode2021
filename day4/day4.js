158797850766d65460163d279d8e3f081328feeb
hizashi <zytwp@hotmail.com>
2022-03-25 23:33:19 +0800

    day4 part1 done]

--- day4/day4.js
+++ day4/day4.js
@@ -5,21 +5,122 @@ var inputData = fs.readFileSync("day4.txt", "utf-8");
 var splitData = inputData.split("\n");
 //array of the numbers drawn
 var drawNumbers = splitData[0].split(",");
-console.log(drawNumbers);
+// console.log(drawNumbers);
 //remove the numbers drawn and the first empty line from the initial data
 splitData.splice(0, 2);
-console.log(splitData);
-//re-organize the card storage data and add arrays for horizontal lines
-// const allCards: string[][][];
-var h1 = splitData[0].split(" ").filter(function (el) { return el != ""; });
-var h2 = splitData[1].split(" ").filter(function (el) { return el != ""; });
-var h3 = splitData[2].split(" ").filter(function (el) { return el != ""; });
-var h4 = splitData[3].split(" ").filter(function (el) { return el != ""; });
-var h5 = splitData[4].split(" ").filter(function (el) { return el != ""; });
-var arr = [h1, h2, h3, h4, h5];
-console.log(arr);
-var v1 = [h1[0], h2[0], h3[0], h4[0], h5[0]];
-console.log(v1);
-// for (let i = 0; i < splitData.length; i++) {
-//     let h1: string[] = splitData[i].split(" ")
-// }
+//re-organize the card storage data
+var allCardsArray = [];
+var createCards = function (arr) {
+    var card = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == "") {
+            // console.log("an empty line was detected");
+            allCardsArray.push(card);
+            card = [];
+            continue;
+        }
+        var horizontal = arr[i].split(" ").filter(function (el) { return el != ""; });
+        card.push(horizontal);
+    }
+};
+createCards(splitData);
+// console.log(allCardsArray);
+var currentNumber = "";
+var winningCard = [];
+//function for checking win condition
+var checkWin = function (singleCardArray) {
+    //check horizontals
+    for (var i = 0; i < singleCardArray.length; i++) {
+        if (singleCardArray[i].every(function (e) { return e === "-"; })) {
+            winningCard = singleCardArray;
+            console.log(winningCard);
+            console.log(currentNumber);
+            return true;
+        }
+    }
+    //check verticals
+    var v1 = [
+        singleCardArray[0][0],
+        singleCardArray[1][0],
+        singleCardArray[2][0],
+        singleCardArray[3][0],
+        singleCardArray[4][0],
+    ];
+    var v2 = [
+        singleCardArray[0][1],
+        singleCardArray[1][1],
+        singleCardArray[2][1],
+        singleCardArray[3][1],
+        singleCardArray[4][1],
+    ];
+    var v3 = [
+        singleCardArray[0][2],
+        singleCardArray[1][2],
+        singleCardArray[2][2],
+        singleCardArray[3][2],
+        singleCardArray[4][2],
+    ];
+    var v4 = [
+        singleCardArray[0][3],
+        singleCardArray[1][3],
+        singleCardArray[2][3],
+        singleCardArray[3][3],
+        singleCardArray[4][3],
+    ];
+    var v5 = [
+        singleCardArray[0][4],
+        singleCardArray[1][4],
+        singleCardArray[2][4],
+        singleCardArray[3][4],
+        singleCardArray[4][4],
+    ];
+    var verticals = [v1, v2, v3, v4, v5];
+    for (var i = 0; i < verticals.length; i++) {
+        if (verticals[i].every(function (e) { return e === "-"; })) {
+            winningCard = singleCardArray;
+            console.log(winningCard);
+            console.log(currentNumber);
+            return true;
+        }
+    }
+    return false;
+};
+//function for finding the current draw number and replacing it with "-"
+var playBingo = function (array, numbers) {
+    //loop through all the draw numbers
+    for (var i = 0; i < numbers.length; i++) {
+        currentNumber = numbers[i];
+        var status_1 = false;
+        //loop through the entire array of cards
+        for (var j = 0; j < array.length; j++) {
+            var currentCard = array[j]; //card at index j
+            //loop through each row in the card
+            for (var k = 0; k < currentCard.length; k++) {
+                var currentRow = currentCard[k];
+                if (currentRow.findIndex(function (e) { return e === currentNumber; }) === -1) {
+                    continue;
+                }
+                else {
+                    var indexOfNumber = currentRow.findIndex(function (e) { return e === currentNumber; });
+                    allCardsArray[j][k][indexOfNumber] = "-";
+                }
+            }
+            status_1 = checkWin(currentCard);
+            if (status_1 == true) {
+                break;
+            }
+        }
+        if (status_1 == true) {
+            break;
+        }
+    }
+};
+playBingo(allCardsArray, drawNumbers);
+// console.log(allCardsArray)
+var flatten = winningCard.flat().filter(function (e) { return e != '-'; });
+console.log(flatten);
+var sum = 0;
+for (var i = 0; i < flatten.length; i++) {
+    sum += parseInt(flatten[i]);
+}
+console.log(currentNumber, parseInt(currentNumber) * sum);
--- day4/day4.ts
+++ day4/day4.ts
@@ -1,44 +1,47 @@
-import { on } from 'events'
-import * as fs from 'fs'
+import { on } from "events";
+import * as fs from "fs";
 
-const inputData: string = fs.readFileSync('day4.txt', 'utf-8')
-const splitData: Array<string> = inputData.split('\n')
+const inputData: string = fs.readFileSync("day4.txt", "utf-8");
+const splitData: Array<string> = inputData.split("\n");
 
 //array of the numbers drawn
-const drawNumbers: string[] = splitData[0].split(',')
-console.log(drawNumbers)
+const drawNumbers: string[] = splitData[0].split(",");
+// console.log(drawNumbers);
 
 //remove the numbers drawn and the first empty line from the initial data
-splitData.splice(0, 2)
-console.log(splitData)
+splitData.splice(0, 2);
 
-//re-organize the card storage data and add arrays for horizontal lines
-let cardArray: string[][][];
+//re-organize the card storage data
+let allCardsArray: string[][][] = [];
 
 const createCards = (arr: string[]) => {
-  let card: string[][] = []
+  let card: string[][] = [];
   for (let i = 0; i < arr.length; i++) {
-    let card: string[] = []
-    if (arr[i] == '') {
-      console.log('an empty line was detected')
-      continue
+    if (arr[i] == "") {
+      // console.log("an empty line was detected");
+      allCardsArray.push(card);
+      card = [];
+      continue;
     }
-    let horizontal: string[] = arr[i].split(' ').filter((el) => el != '')
+    let horizontal: string[] = arr[i].split(" ").filter((el) => el != "");
+    card.push(horizontal);
   }
-}
+};
+createCards(splitData);
+// console.log(allCardsArray);
 
-const isDrawn = (currentValue: string) => {
-  currentValue = '-'
-}
-let lastDrawnNumber: string = ''
-let winningCard: string[] = []
+let currentNumber: string = "";
+let winningCard: string[][] = [];
+
+//function for checking win condition
 const checkWin = (singleCardArray: string[][]) => {
   //check horizontals
   for (let i = 0; i < singleCardArray.length; i++) {
-    if (singleCardArray[i].every(isDrawn)) {
-      winningCard = singleCardArray[i]
-      console.log(winningCard)
-      return
+    if (singleCardArray[i].every((e) => e === "-")) {
+      winningCard = singleCardArray;
+      console.log(winningCard);
+      console.log(currentNumber);
+      return true;
     }
   }
 
@@ -49,42 +52,88 @@ const checkWin = (singleCardArray: string[][]) => {
     singleCardArray[2][0],
     singleCardArray[3][0],
     singleCardArray[4][0],
-  ]
+  ];
   let v2: string[] = [
     singleCardArray[0][1],
     singleCardArray[1][1],
     singleCardArray[2][1],
     singleCardArray[3][1],
     singleCardArray[4][1],
-  ]
+  ];
   let v3: string[] = [
     singleCardArray[0][2],
     singleCardArray[1][2],
     singleCardArray[2][2],
     singleCardArray[3][2],
     singleCardArray[4][2],
-  ]
+  ];
   let v4: string[] = [
     singleCardArray[0][3],
     singleCardArray[1][3],
     singleCardArray[2][3],
     singleCardArray[3][3],
     singleCardArray[4][3],
-  ]
+  ];
   let v5: string[] = [
     singleCardArray[0][4],
     singleCardArray[1][4],
     singleCardArray[2][4],
     singleCardArray[3][4],
     singleCardArray[4][4],
-  ]
-  let verticals: string[][] = [v1, v2, v3, v4, v5]
-  console.log(verticals)
+  ];
+  let verticals: string[][] = [v1, v2, v3, v4, v5];
+
   for (let i = 0; i < verticals.length; i++) {
-    if (verticals[i].every(isDrawn)) {
-      winningCard = singleCardArray[i]
-      console.log(winningCard)
-      return
+    if (verticals[i].every((e) => e === "-")) {
+      winningCard = singleCardArray;
+      console.log(winningCard);
+      console.log(currentNumber);
+      return true;
     }
   }
+  return false;
+};
+
+//function for finding the current draw number and replacing it with "-"
+const playBingo = (array: string[][][], numbers: string[]) => {
+  //loop through all the draw numbers
+  for (let i = 0; i < numbers.length; i++) {
+    currentNumber = numbers[i];
+    let status: boolean = false;
+    //loop through the entire array of cards
+    for (let j = 0; j < array.length; j++) {
+      let currentCard: string[][] = array[j]; //card at index j
+      //loop through each row in the card
+      for (let k = 0; k < currentCard.length; k++) {
+        let currentRow: string[] = currentCard[k];
+        if (currentRow.findIndex((e) => e === currentNumber) === -1) {
+          continue;
+        } else {
+          let indexOfNumber: number = currentRow.findIndex(
+            (e) => e === currentNumber
+          );
+          allCardsArray[j][k][indexOfNumber] = "-";
+        }
+      }
+      status = checkWin(currentCard);
+      if (status == true) {
+        break;
+      }
+    }
+    if (status == true) {
+      break;
+    }
+  }
+};
+
+playBingo(allCardsArray, drawNumbers);
+// console.log(allCardsArray)
+
+
+const flatten: string[] = winningCard.flat().filter(e => e != '-')
+console.log(flatten)
+let sum: number = 0
+for (let i = 0; i < flatten.length; i++){
+  sum += parseInt(flatten[i])
 }
+console.log(currentNumber, parseInt(currentNumber)*sum)
